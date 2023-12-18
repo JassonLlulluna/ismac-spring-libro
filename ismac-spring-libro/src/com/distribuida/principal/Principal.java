35fdea6163797c183d62f623fc9ee9cc522634c4
JassonLlulluna <150932483+JassonLlulluna@users.noreply.github.com>
2023-12-18 10:25:36 -0500

    upt

--- ismac-spring-libro/src/com/distribuida/principal/Principal.java
+++ ismac-spring-libro/src/com/distribuida/principal/Principal.java
@@ -21,56 +21,57 @@ public class Principal {
 		
 		
 		
-		//INSTANCIAR
-		Cliente cliente = context.getBean("idCliente",Cliente.class);
-		Libro libro = context.getBean("idLibro",Libro.class);		     //CODIGO ACOPLADO
-		Factura factura = context.getBean("idFactura",Factura.class);	
-		DetalleFactura facturaDetalle = context.getBean("idFacturaDetalle",DetalleFactura.class);
-		Categoria categoria = context.getBean("idCategoria",Categoria.class);
-		Autor autor = context.getBean("idAutor",Autor.class);
+		//INSTANCIAR               //En anotaciones es el nombre de la clase pero primero con minuscula
+		Cliente cliente = context.getBean("cliente",Cliente.class);
+		Libro libro = context.getBean("libro",Libro.class);		     //CODIGO ACOPLADO
+		Factura factura = context.getBean("factura",Factura.class);	
+		DetalleFactura facturaDetalle = context.getBean("detalleFactura",DetalleFactura.class);
+		Categoria categoria = context.getBean("categoria",Categoria.class);
+		Autor autor = context.getBean("autor",Autor.class);
 		
 
 		
-		cliente.setId_cliente(1);
+		cliente.setId_Cliente(1);
 		cliente.setNombre("Paul");
 		cliente.setApellido("Apellido");
 		cliente.setCedula("1755414727");
 		cliente.setCorreo("jassonl@gmail");
 		cliente.setDireccion("Tumbaco");
+		cliente.setTelefono("098784858");
 		cliente.setEdad(19);
-		cliente.setFecha_nac(new Date());
+		cliente.setFecha_Nac(new Date());
 		
-		libro.setId_libro(1);
+		libro.setId_Libro(1);
 		libro.setTitulo("Don Quijotes");
 		libro.setEditorial("Editorial");
-		libro.setNum_paginas(1);
+		libro.setNum_Paginas(1);
 		libro.setEdicion("Edicion");
 		libro.setIdioma("Español");
 		libro.setFecha_publicacion(new Date());
 		libro.setDescripcion("Libro original don quijote");
-		libro.setTipo_pasta("Empastado");
+		libro.setTipo_Pasta("Empastado");
 		libro.setIsbn("02030");
-		libro.setNum_ejemplares(100);
+		libro.setNum_Ejemplares(100);
 		libro.setPortada("Portada");
 		libro.setPresentacion("Presentacion");
 		libro.setPrecio(50);
 		
-		factura.setId_factura(1);
+		factura.setId_Factura(1);
 		factura.setFecha(0);
 		factura.setIva(0);
 		factura.setTotal(0);
-		factura.setTotal_neto(0);
+		factura.setTotal_Neto(0);
 		
-		facturaDetalle.setId_detallefactura(1);
+		facturaDetalle.setId_DetalleFactura(1);
 		facturaDetalle.setCantidad(1);
 		facturaDetalle.setSubtotal(1);
 		
-		categoria.setId_categoria(1);
+		categoria.setId_Categoria(1);
 		categoria.setCategoria("Infantil");
 		categoria.setDescripcion("Libro de Don Quijotes");
 		
 		
-		autor.setId_autor(1);
+		autor.setId_Autor(1);
 		autor.setNombre("Don");
 		autor.setApellido("Quijote");
 		autor.setCorreo("donquijote@email.com");
